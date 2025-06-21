bacbb04bb1d295e94e9bb673b0452a1ec3bb5565
VLC <72214168+oodebrecht@users.noreply.github.com>
2025-06-21 15:31:17 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,28 +21,36 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-        {/* Meta Pixel Code (commented) */}
-        {/* 
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'YOUR_PIXEL_ID_HERE');
-            fbq('track', 'PageView');
-          `
-        }} />
+        <link rel="icon" href="https://cdn4.iconfinder.com/data/icons/3d-cyber-security/256/TOPI_IS.png" />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '633116172759767', {
+                external_id: 'EAAa0uAc5UoABOzIZAaZBbhupvvvVHRX1lap0tZBXGuthlK75cKUXBilTfbKo1l0uDmizlOt64sYAp7HrNKki97NfR9yXwbwD6Yc962uPtN4EY6h7K4ZBG7q48pN9ME5CWKejEwSMiTftRBynoRn2bB0ODzXf6tdCNhuqPlZCvYZCvlIjhBQDkN6BUIGgxxFgZDZD'
+              });
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         <noscript>
-          <img height="1" width="1" style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID_HERE&ev=PageView&noscript=1"
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=633116172759767&ev=PageView&noscript=1"
           />
         </noscript>
-        */}
+        {/* End Meta Pixel Code */}
       </head>
       <body className={inter.className}>
         <div className="min-h-screen bg-black">
