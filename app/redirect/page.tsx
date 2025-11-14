4e9b700e9f3e46ab6d4c175fd3e10bc2e45cf3c3
VLC <72214168+oodebrecht@users.noreply.github.com>
2025-11-14 12:19:04 -0300

    Update page.tsx

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -5,12 +5,7 @@ import { useEffect } from "react"
 export default function RedirectPage() {
   useEffect(() => {
     const urls = [
-      "https://t.me/BrazaPacksBot?start=cmbph2ijs04hlnx0s2tbkg3qj",
-      "https://t.me/topdanadinhasbot?start=cmbph5asw04pjnx0s9rjofsih",
-      "https://t.me/MeiaNoveBot?start=cmbph52iq04pcnx0s2my3pkri",
-      "https://t.me/BlazaFilmesBot?start=start",
-      "https://t.me/TheBricsBot?start=start",
-      "https://t.me/addlist/SpWRdb4MCWc1NzVh",
+      "https://clipeporno.com/bot",
       
     ]
     const randomIndex = Math.floor(Math.random() * urls.length)
